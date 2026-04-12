aa2f5dcce742440751ede87e10b8da1a8bd89997
Maulei <maulei@connect.hku.hk>
2026-04-12 22:08:20 +0800

    try to commit

--- backend/src/main/java/com/spendify/backend/SpendifyApplication.java
+++ backend/src/main/java/com/spendify/backend/SpendifyApplication.java
@@ -2,8 +2,6 @@ package com.spendify.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpendifyApplication {
@@ -12,8 +10,5 @@ public class SpendifyApplication {
         SpringApplication.run(SpendifyApplication.class, args);
     }
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+
 }
