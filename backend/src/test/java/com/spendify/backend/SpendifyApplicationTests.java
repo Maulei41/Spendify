da55b9653592dcedc0c5425a58a7cd2d935c5538
Maulei <maulei@connect.hku.hk>
2026-04-12 22:05:34 +0800

    try to commit

--- backend/src/test/java/com/spendify/backend/SpendifyApplicationTests.java
+++ backend/src/test/java/com/spendify/backend/SpendifyApplicationTests.java
@@ -1,12 +1,17 @@
 package com.spendify.backend;
 
+import net.sourceforge.tess4j.ITesseract;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class SpendifyApplicationTests {
+
+    @MockBean
+    private ITesseract tesseract;
 
     @Test
     void contextLoads() {
