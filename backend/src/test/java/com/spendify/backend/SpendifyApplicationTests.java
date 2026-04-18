598030479043cfb974c1148e111525715a820f64
Maulei <maulei@connect.hku.hk>
2026-04-18 08:26:14 +0800

    fix ocr

--- backend/src/test/java/com/spendify/backend/SpendifyApplicationTests.java
+++ backend/src/test/java/com/spendify/backend/SpendifyApplicationTests.java
@@ -1,17 +1,17 @@
 package com.spendify.backend;
 
-import net.sourceforge.tess4j.ITesseract;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class SpendifyApplicationTests {
-
-    @MockBean
-    private ITesseract tesseract;
 
     @Test
     void contextLoads() {
